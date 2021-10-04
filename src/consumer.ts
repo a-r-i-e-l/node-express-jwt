1f0098324c5c619cb924cb427d7823b53153d85d
Ariel <ariel@elsa.science>
2021-10-04 15:59:01 +0200

    Removing unnecesary fetchings and adding hashing

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,32 +1,23 @@
 import express, {json} from 'express'
 import fetch from 'node-fetch'
 import * as jwt from 'jsonwebtoken'
+import * as crypto from 'crypto'
 
 const SECRET_KEY = 'secretKey'
 
 export const consumerApp = express()
 
-consumerApp.get('/test1', async (req, res) => {
-  const test = await fetch('https://api.thecatapi.com/v1/images/search?breed_id=beng', {
-    method: 'GET',
-  })
-  // res.json(await test.json())
-  res.send('<pre><code>' + JSON.stringify(await test.json(), null, 4) + '</code></pre>')
-})
-
-consumerApp.get('/test2', async (req, res) => {
-  const test = await fetch('http://localhost:3003/test', {
-    method: 'GET',
-  })
-  // res.json(await test.json())
-  res.send('<pre><code>' + JSON.stringify(await test.json(), null, 4) + '</code></pre>')
-})
-
 consumerApp.get('/', async (req, res) => {
   const payload = {
     data: 'Ariel',
+    foo: 'bar',
   }
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '5s'})
+
+  const body = JSON.stringify(payload)
+
+  const hash = crypto.createHash('sha1').update(body).digest('hex')
+
+  const token = jwt.sign({hash}, SECRET_KEY, {expiresIn: '5s'})
   console.log({token})
 
   const test = await fetch('http://localhost:3003/api', {
@@ -35,7 +26,7 @@ consumerApp.get('/', async (req, res) => {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(payload),
+    body,
   })
   // res.json(await test.json())
   res.send('<pre><code>' + JSON.stringify(await test.json(), null, 4) + '</code></pre>')
